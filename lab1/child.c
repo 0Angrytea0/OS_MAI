d3ad58d8410701401181909347b69f49e51463e6
0Angrytea0 <116645263+0Angrytea0@users.noreply.github.com>
2023-12-14 14:51:41 +0300

    Create child.c

---
+++ lab1/child.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+int main() {
+    char _string[256];
+    int bytesRead;
+
+    bytesRead = read(STDIN_FILENO, _string, sizeof(_string));
+    
+    if (_string[bytesRead - 2] == ';' || _string[bytesRead - 2] == '.') {
+        // строка заканчивается на ";" или "."
+        write(STDOUT_FILENO, _string, bytesRead - 1);
+    } else {
+        // строка не соответствует условию
+        write(STDERR_FILENO, "Ошибка: строка не заканчивается на \";\" или \".\"\n", 80);
+        exit(-1);
+    }
+
+    return 0;
+}
