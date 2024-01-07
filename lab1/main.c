2dd020dab762f616c895768aaa73be6726c32bd4
0Angrytea0 <116645263+0Angrytea0@users.noreply.github.com>
2024-01-08 02:27:19 +0600

    Update main.c

--- lab1/main.c
+++ lab1/main.c
@@ -7,24 +7,29 @@
 
 int main() {
     char FileName[256];
-    write(STDOUT_FILENO, "Введите имя файла для записи: ", 55);
-    read(STDIN_FILENO, FileName, 256);
+    if(write(STDOUT_FILENO, "Введите имя файла для записи: ", 55) < 0){
+        exit(-1);
+    }
+    if((read(STDIN_FILENO, FileName, 256)) < 0){
+        exit(-1);
+    }
     FileName[strlen(FileName) - 1] = '\0'; 
     int file = open(FileName, O_WRONLY); 
     if(file < 0) {
-        perror("Error file");
+        printf("Opening error file\n");
         exit(-1);
     }
-
     int pipe1[2], pipe2[2];
-    pipe(pipe1);
-    pipe(pipe2);
+    if((pipe(pipe1)) < 0 || (pipe(pipe2)) < 0){
+        printf("Error pipe\n");
+        exit(-1);
+    }
 
     pid_t child_pid = fork();
     
     if (child_pid == -1)
     {
-        write(STDERR_FILENO, "fork", 5);
+        printf("fork");
         exit(-1);
     }
 
@@ -33,10 +38,17 @@ int main() {
         close(pipe1[1]); //na zapis
         close(pipe2[0]); //na chtenie
 
-        dup2(pipe1[0], STDIN_FILENO);
-        dup2(pipe2[1], STDOUT_FILENO);
+        if ((dup2(pipe1[0], STDIN_FILENO)) < 0){
+            printf("error dup2\n");
+            exit(-1);
+        }
+        if ((dup2(pipe2[1], STDOUT_FILENO)) < 0){
+            printf("error dup2\n");
+            exit(-1);
+        }
 
-        execlp("./child", "./child", NULL);
+        execlp("./child.out", "./child.out", NULL);
+        perror("execlp\n");
         _exit(1);
     } else if(child_pid > 0) {
         // Родительский процесс
@@ -45,12 +57,22 @@ int main() {
 
         char _String[256];
 
-        write(STDOUT_FILENO, "Введите строку: ", 30);
+        if((write(STDOUT_FILENO, "Введите строку: ", 30)) < 0){
+            exit(-1);
+        }
         int bytesRead = read(STDIN_FILENO, _String, sizeof(_String));
+        if (bytesRead < 0){
+            exit(-1);
+        }
 
-        write(pipe1[1], _String, bytesRead);
+        if((write(pipe1[1], _String, bytesRead)) < 0){
+            exit(-1);
+        }
             
         int bytesReadChild = read(pipe2[0], _String, sizeof(_String));
+        if (bytesRead < 0) {
+            exit(-1);
+        }
         
         int status;
         waitpid(0, &status, 0);
@@ -58,9 +80,9 @@ int main() {
         if(status != 0)
             return -1;
         else {   
-            write(STDOUT_FILENO, "Строка удовлетворяет условию: ", 57);
-            write(STDOUT_FILENO, _String, bytesReadChild);
-            write(STDOUT_FILENO, "\n", 2);
+            if((write(STDOUT_FILENO, "Строка удовлетворяет условию: ", 57)) < 0 || (write(STDOUT_FILENO, _String, bytesReadChild)) < 0 || (write(STDOUT_FILENO, "\n", 2)) < 0){
+                exit(-1);
+            }
         }
 
         close(pipe1[1]);
